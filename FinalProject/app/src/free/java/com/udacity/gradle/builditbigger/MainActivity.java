9f2735fcfbd8be54ae4ce1be3ecd7ea23bda4ebb
dqshare <739362790@qq.com>
2016-11-02 21:34:46 +0800

    add interstitial ad

---
+++ FinalProject/app/src/free/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -0,0 +1,92 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+
+import cn.ben.JokeDisplayActivity;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private static final String MY_AD_UNIT_ID = "ca-app-pub-3940256099942544/1033173712";
+    private InterstitialAd mInterstitialAd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Create the mInterstitialAd.
+        mInterstitialAd = new InterstitialAd(MainActivity.this);
+        mInterstitialAd.setAdUnitId(MY_AD_UNIT_ID);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void tellJoke(View view) {
+        // should check whether a task is running
+        new GetJokeEndpointsAsyncTask(new GetJokeEndpointsAsyncTask.OnTaskFinishedListener() {
+            @Override
+            public void handle(final String result) {
+                // Create ad request.
+                AdRequest adRequest = new AdRequest.Builder().build();
+                // Begin loading your mInterstitialAd.
+                mInterstitialAd.loadAd(adRequest);
+                mInterstitialAd.setAdListener(new AdListener() {
+                    @Override
+                    public void onAdFailedToLoad(int i) {
+                        super.onAdFailedToLoad(i);
+                        Toast.makeText(MainActivity.this, "Check your network!", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onAdLoaded() {
+                        super.onAdLoaded();
+                        mInterstitialAd.show();
+                    }
+
+                    @Override
+                    public void onAdClosed() {
+                        super.onAdClosed();
+
+                        Intent intent = new Intent(MainActivity.this, JokeDisplayActivity.class);
+                        intent.putExtra(JokeDisplayActivity.EXTRA_JOKE, result);
+                        startActivity(intent);
+                    }
+                });
+            }
+        }).execute(this);
+    }
+
+
+}
